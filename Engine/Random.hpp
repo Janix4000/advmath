1d11e621a0e67dba8566de879be20a161492f9fb
Janix4000 <janix4000@gmail.com>
2019-06-05 13:08:50 +0200

    Added effolkronium-random library

---
+++ Engine/Random.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <effolkronium/random.hpp>
+
+// get base random alias which is auto seeded and has static API and internal state
+using Random = effolkronium::random_static;
